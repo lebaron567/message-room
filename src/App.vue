585a03ebdc4eb6d4b2fcae9effa82058f8041e49
Evan Skp <evanskp@MacBook-Air-de-Evan.local>
2025-04-24 14:35:41 +0200

    debut creation salon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+    import createsalon from './components/CreateSalon/createsalon.vue';
 </script>
 
 <template>
-
+    <div id="app">
+        <createsalon />
+    </div>
 </template>
 
 <style scoped>
---
+++ src/components/CreateSalon/createsalon.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import './createsalon.css';
+</script>
+<template>
+    <div class="create-salon">
+        <h1>Create a New Salon</h1>
+        <form @submit.prevent="createSalon">
+            <div>
+                <label for="name">Salon Name:</label>
+                <input type="text" v-model="salonName" required />
+            </div>
+            <div>
+                <label for="location">Image :</label>
+                <input type="text" v-model="imageUrl" required />
+            </div>
+            <div>
+                <label for="location">Thème color :</label>
+                <input type="color" v-model="themeColor" required />
+            </div>
+            <button type="submit">Create Salon</button>
+        </form>
+    </div>
+    <div v-if="error" class="error">{{ error }}</div>
+    <div v-if="success" class="success">Salon created successfully!</div>
+</template>
+
