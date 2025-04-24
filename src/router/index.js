22c35469855b623d1f19ce6eac98da172fc8d2f5
arthurLacombe <arthurlacombe44@gmail.com>
2025-04-24 15:11:05 +0200

    finish channel list and images for channels

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '@/components/LoginForm.vue'
 import HomeView from '@/views/HomeView.vue'
+import SalonList from '@/components/SalonList.vue'
 
 const routes = [
   { path: '/', name: 'login', component: LoginForm },
   { path: '/home', name: 'home', component: HomeView },
+  { path: '/channels', name: 'channels', component: SalonList}
 ]
 
 const router = createRouter({
